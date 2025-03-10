e419040da49a1f7ddcf84a5db29c5596c0db51fc
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-03-10 15:28:43 +0100

    Création de la classe Entity + mise à jour des autres classes pour l'utiliser

--- factory/app/Entity/Bicycle.php
+++ factory/app/Entity/Bicycle.php
@@ -2,7 +2,8 @@
 
 namespace App\Entity;
 
-class Bicycle {
+class Bicycle implements Entity
+{
     private $costPerKm;
     private $fuelType;
 
@@ -11,11 +12,11 @@ class Bicycle {
         $this->fuelType = $fuelType;
     }
 
-    public function getCostPerKm() {
+    public function getCostPerKm(): float {
         return $this->costPerKm;
     }
 
-    public function getFuelType() {
+    public function getFuelType(): string {
         return $this->fuelType;
     }
 }
