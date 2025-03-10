cb1ade5a3a19b1e5a7503854a1a6e809665830fc
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-03-10 15:47:30 +0100

    Création de classe Config avec deux méthodes et aussi qui suit le design pattern Singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -1,5 +1,28 @@
 <?php
 
-# TODO: Créer une classe Config en Singleton
-
 namespace App;
+
+class Config
+{
+    private static ?Config $instance = null;
+    private array $settings;
+
+    private function __construct()
+    {
+        $this->settings = require __DIR__ . '/../config/config.php';
+    }
+
+    public static function getInstance(): Config
+    {
+        if (self::$instance === null) {
+            self::$instance = new Config();
+        }
+
+        return self::$instance;
+    }
+
+    public function get(string $key)
+    {
+        return $this->settings[$key] ?? null;
+    }
+}
